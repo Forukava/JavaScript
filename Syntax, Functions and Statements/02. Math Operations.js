c8f94969c051fd95eaa940a10c726f997503cae7
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-07 09:05:06 +0200

    Create 02. Math Operations.js

---
+++ Syntax, Functions and Statements/02. Math Operations.js
@@ -0,0 +1,25 @@
+function solve(num1, num2, sign){
+    let result = 0
+switch (sign) {
+    case '+':
+        result = num1 + num2
+        break;
+    case '-':
+        result = num1 - num2
+        break;
+    case '/':
+        result = num1 / num2
+        break;
+    case '*':
+        result = num1 * num2
+        break;
+    case '%':
+        result = num1 % num2
+        break;
+    case "**":
+        result = num1 ** num2
+        break;
+}
+console.log(result)
+}
+solve(5, 6, '**')
