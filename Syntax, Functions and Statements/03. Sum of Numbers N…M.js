0e63a0ac4cc35bc2994098c5fde60da970d27054
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-07 10:21:55 +0200

    Create 03. Sum of Numbers N…M.js

---
+++ Syntax, Functions and Statements/03. Sum of Numbers N…M.js
@@ -0,0 +1,10 @@
+function solve(num1, num2){
+let first = Number(num1)
+let secound = Number(num2)
+let result = 0
+for(let i = first; i <= secound; i++){
+    result += i
+   }
+console.log(result)
+}
+solve('1', '5')
