2098e11dd021b814ee90bf4a8141bc43af00ffd7
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-15 14:09:26 +0200

    Update 06. Road Radar.js

--- Exercise Syntax, Functions and Statements/06. Road Radar.js
+++ Exercise Syntax, Functions and Statements/06. Road Radar.js
@@ -1,4 +1,35 @@
-function solve(){
+function solve([speed, area]){
 
+
+let covl = (limit, speed) => {
+    if(limit >= speed){
+        console.log()
+        return
+    }
+    let overLimit = speed - limit
+    if(overLimit <= 20){
+        console.log('speeding')
+    }else if (overLimit <= 40){
+        console.log('excessive speeding')
+    }else if(overLimit > 40){
+        console.log('reckless driving')
+    }
 }
-solve([40, 'city'])
+switch(area){
+    case 'motorway':
+        covl(130,speed)
+        break;
+    case 'interstate':
+            covl(90,speed)
+            break;
+    case 'city':
+        covl(50,speed)
+        break;
+    case 'residential':
+        covl(20,speed)
+        break;
+    default:
+        break;
+    }
+}
+solve([30, 'city'])
