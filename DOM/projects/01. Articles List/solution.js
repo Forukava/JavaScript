3f1241b8c9f8722e3dad0222b6e589a52a2887ab
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-19 16:01:20 +0200

    Update solution.js

--- DOM/projects/01. Articles List/solution.js
+++ DOM/projects/01. Articles List/solution.js
@@ -1,4 +1,16 @@
 function createArticle() {
-	
-	//TODO...
+	let article = document.createElement('article')
+	let h3 = document.createElement('h3')
+	let p = document.createElement('p')
+
+	let input = document.getElementById('createTitle')
+	let textarea = document.getElementById('createContent')
+	let articles = document.getElementById('articles')
+
+	h3.innerHTML = input.value;
+	 p.innerHTML = textarea.value;
+
+	 article.appendChild(h3)
+	 article.appendChild(p)
+	 articles.appendChild(article)
 }
