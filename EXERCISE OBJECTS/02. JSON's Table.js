5fce81a3d8874a0668c78f717b656163123aa72c
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-23 15:44:21 +0200

    Update 02. JSON's Table.js

--- EXERCISE OBJECTS/02. JSON's Table.js
+++ EXERCISE OBJECTS/02. JSON's Table.js
@@ -2,15 +2,18 @@ function htmlTable(data){
     let parseData = data.map(x => JSON.parse(x))
 
     let createTable = content => `<table>\n${content}\n</table>`
-    let createRow = content => `<table>\n  ${content}\n</table>`
-    let createData = content => `<table>\n    ${content}\n</table>`
+    let createRow = content => `<tr>\n  ${content}\n</tr>`
+    let createData = content => `<td>\n    ${content}\n</td>`
 
-   parseData.reduce((accRows, row)=>{
-    console.log(row.name)
-    Object.value(row).reduce((tdAcc, td) =>{
-        return tdAcc + createData(td)
-    })
-   } , '')
+    console.log(createTable('Im table'))
+    console.log(createRow('I m Row'))
+    console.log(createData('Im data'))
+   //parseData.reduce((accRows, row)=>{
+    //console.log(row.name)
+    //Object.value(row).reduce((tdAcc, td) =>{
+        //return tdAcc + createData(td)
+    //})
+  // } , '')
 }
 htmlTable(['{"name":"Pesho","position":"Promenliva","salary":100000}',
 '{"name":"Teo","position":"Lecturer","salary":1000}',
