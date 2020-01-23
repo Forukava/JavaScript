3811706e0a9a437a4266c9bd2404e7620ba591aa
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-23 12:51:01 +0200

    Update 01. Heroic Inventory.js

--- EXERCISE OBJECTS/01. Heroic Inventory.js
+++ EXERCISE OBJECTS/01. Heroic Inventory.js
@@ -1,12 +1,23 @@
-function solve(input){
- return parseData = input.reduce((acc, curr) => {
-     let [name, level, items] = curr.split(' / ')
-    acc.push({name, level: Number(level), items: items ? items.split(',').map(x=>x.trim()
-}), [])
- return JSON.stringify(parseData)
-}
+function heroicInventory(input) {
+    let heroes = [];
 
- console.log(solve(['Isacc / 25 / Apple, GravityGun',
+    for(let line of input) {
+        let tokens = line.split(/\s*\/\s*/);
+        let name = tokens[0];
+        let level = Number(tokens[1]);
+        let items = [];
+
+        if(tokens.length > 2) {
+            items = tokens[2].split(", ");
+        }
+
+        let hero = {name: name, level: level, items: items};
+        heroes.push(hero);
+    }
+
+    console.log(JSON.stringify(heroes));
+}
+heroicInventory(['Isacc / 25 / Apple, GravityGun',
 'Derek / 12 / BarrelVest, DestructionSword',
 'Hes / 1 / Desolator, Sentinel, Antara']
-))
+)
