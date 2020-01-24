290ce7de192ce16fa6399e9c41d9b1de2e88a1f2
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-24 14:18:40 +0200

    1

--- EXERCISE OBJECTS/04. Store Catalogue.js
+++ EXERCISE OBJECTS/04. Store Catalogue.js
@@ -1,10 +1,28 @@
-function boo(a){
- let parse = a.reduce((acc, product) => {
+function storeCatalogue(input) {
+    let map = new Map();
 
- } , {})
- console.log(parse)
+    for(let line of input){
+        let tokens = line.split(" : ");
+        let product = tokens[0];
+        let price = tokens[1];
+        map.set(product, price);
+    }
+
+    let initials = new Set();
+    Array.from(map.keys()).forEach(k => initials.add(k[0]));
+
+
+    for(let char of Array.from(initials.keys()).sort()) {
+        console.log(char);
+
+        for(let product of Array.from(map.keys()).sort()){
+            if(product.startsWith(char)) {
+                console.log(`  ${product}: ${map.get(product)}`);
+            }
+        }
+    }
 }
-boo(['Appricot : 20.4',
+storeCatalogue(['Appricot : 20.4',
 'Fridge : 1500',
 'TV : 1499',
 'Deodorant : 10',
