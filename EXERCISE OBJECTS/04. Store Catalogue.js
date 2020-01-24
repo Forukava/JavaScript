5b20d561c11e385d58979f49e329f448318d8d99
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-24 13:22:05 +0200

    Update 04. Store Catalogue.js

---
+++ EXERCISE OBJECTS/04. Store Catalogue.js
@@ -0,0 +1,14 @@
+function boo(a){
+ let parse = a.reduce((acc, product) => {
+
+ } , {})
+ console.log(parse)
+}
+boo(['Appricot : 20.4',
+'Fridge : 1500',
+'TV : 1499',
+'Deodorant : 10',
+'Boiler : 300',
+'Apple : 1.25',
+'Anti-Bug Spray : 15',
+'T-Shirt : 10'])
