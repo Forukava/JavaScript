13f7739e1f397f6afe2b1016c3a1cbe53ec2f48c
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-25 09:52:45 +0200

    Update 01. Sort Array.js

--- Exercise Advanced Functions/01. Sort Array.js
+++ Exercise Advanced Functions/01. Sort Array.js
@@ -1,5 +1,7 @@
 function sort(input, arg){
-    let result = input.sort((a,b)=> a-b)
-    console.log(result)
+    let result = input.sort((a,b)=> arg === 'asc' ?  a-b : b-a)
+return result
+
+    
 }
 sort([14, 7, 17, 6, 8], 'asc')
