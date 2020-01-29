10017d45f65b438d249f57a6a1ec2630f0d5b1c7
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-29 13:01:09 +0200

    1

---
+++ 12. Exercise DOM/01. Chat-Room/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+    let sendButton = document.getElementById('send');
+    let input = document.getElementById('chat_input');
+    let messafeField = document.getElementById('chat_messages')
+    
+    sendButton.addEventListener('click', (e) =>{
+      
+      console.log(input.value)
+
+      let newElement = document.createElement('div')
+      newElement.innerHTML = input.value;
+      newElement.classList.add('message', 'my-message') 
+
+      messafeField.appendChild(newElement)
+      input.value = ''
+    })
+}
+
+
