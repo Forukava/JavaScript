96a944c543ab302dc98acd8dfea3e7c69cc3694c
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-31 10:47:05 +0200

    Update solution.js

--- 12. Exercise DOM/03. JavaScript-Quizz/solution.js
+++ 12. Exercise DOM/03. JavaScript-Quizz/solution.js
@@ -1,5 +1,7 @@
 function solve() {
-  let answers = document.querySelectorAll('quiz-answer low value').addEventListener('click')
+  let answers = document
+  .querySelectorAll('quiz-answer')
+  .addEventListener('click')
 
   
 }
