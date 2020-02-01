02b826b3b2b35dd9187a428b1f8ca8c7e93f336a
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-02-01 11:09:33 +0200

    Update solution.js

--- 12. Exercise DOM/04. Numpad-Calculator/solution.js
+++ 12. Exercise DOM/04. Numpad-Calculator/solution.js
@@ -1,4 +1,25 @@
 function solve() {
+    let pad = document.getElementsByClassName('keys')[0];
+    let output = document.getElementById('expressionOutput');
+    let result = document.getElementById('resultOutput');
+    let clearButton = document.getElementsByClassName('clear')[0];
 
-    //TODO...
+    let operators = ['+', '-', '/', '*']
+
+    clearButton.addEventListener('click', () =>{
+        output.innerHTML = '';
+        result.innerHTML = '';
+    })
+
+    pad.addEventListener('click',({target: {value}})=>{
+       if(!value){
+           return
+       }
+       if(operators.includes(value)){
+       output.innerHTML = output.innerHTML + `${value}`
+           return
+    } 
+        output.innerHTML = output.innerHTML + value 
+        console.log(value)
+})   
 }
