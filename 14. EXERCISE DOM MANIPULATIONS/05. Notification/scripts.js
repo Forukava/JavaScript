59e492f06ccfd6731709a660d09593bbac1f009e
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-02-06 20:31:53 +0200

    Update scripts.js

--- 14. EXERCISE DOM MANIPULATIONS/05. Notification/scripts.js
+++ 14. EXERCISE DOM MANIPULATIONS/05. Notification/scripts.js
@@ -1,3 +1,9 @@
 function notify(message) {
-    console.log('TODO:...');
+    let div = document.getElementById('notification');
+    div.textContent = message;
+    div.style.display = 'block';
+
+   setTimeout(function (message) {
+    div.style.display = 'none';
+   },2000);
 }
