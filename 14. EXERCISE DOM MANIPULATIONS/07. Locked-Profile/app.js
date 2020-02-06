b62045a24e7ff338d52c84f7d685d3d10397bf0b
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-02-06 20:31:19 +0200

    1

--- 14. EXERCISE DOM MANIPULATIONS/07. Locked-Profile/app.js
+++ 14. EXERCISE DOM MANIPULATIONS/07. Locked-Profile/app.js
@@ -1,3 +1,11 @@
 function lockedProfile() {
-    console.log('TODO...')
-}
+    const buttons = document.querySelectorAll("#main > div > button");
+  [...buttons]
+  .forEach((button) => {
+      button.addEventListener('click', (event) => {
+           const divSibling = event.currentTarget.parentNode.children[9]
+           divSibling.style.display = 'block'
+           console.log(divSibling.id)
+      })
+  });
+} 
